80ee56460b291efa2b3dc21ea980d81a16a8e2a0
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-07 14:12:11 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	/*string initialization*/
 	char Str[] = "\"Programming is like building a multilingual puzzle";
 	puts(Str);
 	return (0);
