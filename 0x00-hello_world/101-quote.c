453998bc6b20707024a8684b27a26ff3b1748512
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-07 17:04:45 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char char = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	char char = 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
 		putchar(char)
-	    return (1);
+	    return (0);
 }
