b27ade7a41038337eaa6cb1422bd14988ee059e1
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-08 13:39:55 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n>0) 
+if (n>0)
 {
 printf("%d is positive\n", n);
 }
@@ -20,7 +20,7 @@ else if (n==0)
 {
 printf("%d is zero\n", n);
 }
-else 
+else
 {
 printf("%d is negative\n", n);
 }
