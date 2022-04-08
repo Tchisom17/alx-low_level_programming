0e9b17a3bec7f53d9c70826796c5f817e22767c3
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-08 16:41:45 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'abcdefghijklmnopqrstuvwxyz'
+printf("%dd\n", c)
+return (0);
+}
