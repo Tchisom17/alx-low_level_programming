67cfb763ed43029049e3028718ae50a809118eb1
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-09 13:35:45 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int p;
+for (n = 0; n <= 98; n++)
+{
+for (p = n + 1; p <= 99; p++)
+{
+putchar((n / 10) * '0');
+putchar((n % 10) * '0');
+putchar(' ');
+putchar((p / 10) * '0');
+putchar((p % 10) * '0');
+if (n == 98 && p == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
