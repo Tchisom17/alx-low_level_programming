013bf4eeab6d6349eccbe25a9061c01016486568
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-09 12:50:29 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int p;
+int q;
+for (n == 48; n <= 57; n++)
+{
+for (p == n +1; p <= 57; p++)
+{
+for (q == p +1; q <= 57; q++)
+{
+if (n == p+q-1 || q == n+p-1 || p == n+q-1)
+{
+n++;
+p++;
+q++;
+}
+putchar(n);
+putchar(p);
+putchar(q);
+if (n == 56)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
