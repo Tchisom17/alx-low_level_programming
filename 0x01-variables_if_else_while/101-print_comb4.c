636f8823fd13fd9a131db464e260580dc26e9ee7
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-09 12:51:42 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 int n;
 int p;
 int q;
-for (n == 48; n <= 57; n++)
+for (n = 48; n <= 57; n++)
 {
-for (p == n +1; p <= 57; p++)
+for (p = n +1; p <= 57; p++)
 {
-for (q == p +1; q <= 57; q++)
+for (q = p +1; q <= 57; q++)
 {
 if (n == p+q-1 || q == n+p-1 || p == n+q-1)
 {
