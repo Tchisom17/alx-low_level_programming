19d0a10f16894e1598d8f16f6e99f071ee6d8888
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-13 12:21:38 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,14 +4,24 @@
  */
 void times_table(void)
 {
-int a,b;
-for (a = 48; a <= 57; a++)
+int a, b, c;
+for (a = 0; a <= 9; a++)
 {
-for (b = 48; b <= 57; b++)
+_putchar('0');
+for (b = 1; b <= 9; b++)
 {
-_putchar(a * b);
 _putchar(',');
 _putchar(' ');
+c = a * b;
+if (c <= 9)
+{
+_putchar(' ');
+}
+else
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
 }
 _putchar('\n');
 }
