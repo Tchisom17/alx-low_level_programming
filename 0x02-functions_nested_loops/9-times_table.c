25cb2f2227f3f58d444f1a2eb4083a2d51b17d65
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-13 13:12:29 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,15 +13,13 @@ for (b = 1; b <= 9; b++)
 _putchar(',');
 _putchar(' ');
 c = a * b;
+
 if (c <= 9)
-{
 _putchar(' ');
-}
 else
-{
 _putchar((c / 10) + '0');
+
 _putchar((c % 10) + '0');
-}
 }
 _putchar('\n');
 }
