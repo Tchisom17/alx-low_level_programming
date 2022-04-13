1f3b86c9ec76c32d7da062ba8b721d3bf0f59cc7
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-13 07:00:25 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints all the letters of the alphabets
+ */
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+}
