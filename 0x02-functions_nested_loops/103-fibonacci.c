462c76f1f9e04b8c0262ae32a2c5faf697ee2a2c
Chisom Amadi <kamsiyochukwu1@gmail.com>
2022-04-13 17:25:03 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always success(0)
+ */
+int main(void)
+{
+unsigned long a = 0, b = 1, sum;
+float total;
+while (1)
+{
+sum = a + b;
+if (sum > 4000000)
+break;
+if ((sum % 2) == 0)
+total += sum;
+a = b;
+b = sum;
+}
+printf("%.0f\n", total);
+return (0);
+}
